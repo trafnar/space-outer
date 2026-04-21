c92e1b087c002d76be75f76d291737f0974002ae
Nathan <nathan@flinto.com>
2026-04-21 09:15:58 -0700

    Update home page with dummy content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World.</h1>
+      <header>
+        <div>Perimeter & area</div>
+        <div>Chapter 11 Mid-Chapter Checkpoint</div>
+      </header>
+
+      <div>
+        <div>Korra’s Score</div>
+        <div>
+          <strong>12</strong> out of <strong>46</strong> points
+        </div>
+      </div>
+
+      <div>
+        {/* left side */}
+        <div>
+          <div>
+            <div>1</div>
+            <div>Select yes or no</div>
+            <div>x 0/2</div>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div>Question 1</div>
+      </div>
     </div>
   );
 }
